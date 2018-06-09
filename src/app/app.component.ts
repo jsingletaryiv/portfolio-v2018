99cf5a5199d10228dc4e4ee6e689fd1f0f854c50
James Singletary <jsingletaryiv@gmail.com>
2018-06-09 14:33:28 -0400

    Inital commit - See README.md for project details.

---
+++ src/app/app.component.ts
@@ -0,0 +1,35 @@
+import { Component, ViewChild, ChangeDetectorRef, OnDestroy } from '@angular/core';
+import { MatMenuTrigger } from '@angular/material/menu';
+import { MediaMatcher } from '@angular/cdk/layout';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+
+export class AppComponent implements OnDestroy {
+  myName = 'James Singletary IV';
+  myLocation = 'Tampa, Florida';
+  appTitle = 'James Singletary\'s Online Portfolio';
+  appSubTitle = 'Welcome to my tiny footprint on the Internet:';
+  mediaQuery: MediaQueryList;
+
+  private _mediaQueryListener: () => void;
+
+  @ViewChild(MatMenuTrigger) trigger: MatMenuTrigger;
+
+  someMethod() {
+    this.trigger.openMenu();
+  }
+
+  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+    this.mediaQuery = media.matchMedia('(max-width: 600px)');
+    this._mediaQueryListener = () => changeDetectorRef.detectChanges();
+    this.mediaQuery.addListener(this._mediaQueryListener);
+  }
+
+  ngOnDestroy(): void {
+    this.mediaQuery.removeListener(this._mediaQueryListener);
+  }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MaterialModule } from './material/material.module';
+import { AppRoutingModule } from './app-routing.module';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MaterialModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
