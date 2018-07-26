f3acb493053575b85f5c68e01bdf8c0e6fe4afef
James Singletary <jsingletaryiv@gmail.com>
2018-07-26 13:51:50 -0400

    Not finding all the icons that I may need within Material therefore installed packages for fontawesome solid SVG for both base and brands sets - Tweaked the navbar styles as well as some page.components

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,18 +4,29 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { CoreRoutingModule } from './core-routing.module';
 import { SharedModule } from '@app/shared';
+// Fontawesome / SVG core library
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+// Icons to import
+import { faDownload } from '@fortawesome/free-solid-svg-icons';
+import { faEllipsisV } from '@fortawesome/free-solid-svg-icons';
+import { faFileDownload } from '@fortawesome/free-solid-svg-icons';
+import { faToolbox } from '@fortawesome/free-solid-svg-icons';
 
 import { NavbarComponent } from './navbar/navbar.component';
-
-/* TODO:: Generate a utils.module that will contain various
-  directives to help facilitate System / Admin requests */
+// TODO:: Generate a utils.module that will contain various
+// directives to help facilitate System / Admin requests
 import { environment } from '../../environments/environment';
+
+// Add icons to the library for convenient access in other components
+library.add(faEllipsisV, faFileDownload, faDownload, faToolbox);
 
 @NgModule({
   imports: [
     CommonModule,
     BrowserAnimationsModule,
     CoreRoutingModule,
+    FontAwesomeModule,
     SharedModule
   ],
   declarations: [
@@ -23,6 +34,7 @@ import { environment } from '../../environments/environment';
   ],
   exports: [
     CoreRoutingModule,
+    FontAwesomeModule,
     NavbarComponent
   ],
   providers: []
